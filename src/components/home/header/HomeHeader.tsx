65f76d0124b428f1c0c2fc620a62d12e5c71a8fe
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-05-16 21:34:57 +0430

    feat: implement Search by country name

--- src/components/home/header/HomeHeader.tsx
+++ src/components/home/header/HomeHeader.tsx
@@ -1,13 +1,36 @@
+import { useState, useEffect, useRef } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import CustomSelect from "../../custom-select/CustomSelect";
 import { regionsList } from "./regionsList";
+import { useNavigate } from "react-router-dom";
 import "./home-header.scss";
 
 const HomeHeader = () => {
+   const [search, setSearch] = useState<string>("");
+   const inputRef = useRef<HTMLInputElement>(null);
+   const navigate = useNavigate();
+
+   useEffect(() => {
+      if (search !== "") {
+         const timer = setTimeout(() => {
+            if (inputRef.current?.value === search) {
+               navigate(`/?name=${search}`);
+            }
+         }, 3000);
+      }
+      return () => {};
+   }, [search]);
+
    return (
       <div className="home-header">
          <div className="home-header__search">
-            <input type="text" placeholder="Search for a country" />
+            <input
+               ref={inputRef}
+               type="text"
+               placeholder="Search for a country"
+               value={search}
+               onChange={(e) => setSearch(e.target.value)}
+            />
             <AiOutlineSearch />
          </div>
          <div className="home-header__region">
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -4,15 +4,24 @@ import CountriesList from "./list/CountriesList";
 import HomeHeader from "./header/HomeHeader";
 import "./home.scss";
 import { ICountry } from "../../typings/countryTypes";
+import { useSearchParams } from "react-router-dom";
 
 const HomeComponent = () => {
    const [counteries, setCounteries] = useState<ICountry[]>([]);
    const [loading, setLoading] = useState<boolean>(false);
+   let [searchParams, setSearchParams] = useSearchParams();
+   console.log("HomeComponent");
+
+   const nameQueryParam = searchParams.get("name");
 
    const getCounteries = async (): Promise<any> => {
+      let dynamicURL = "/all";
+      if (nameQueryParam) {
+         dynamicURL = `/name/${nameQueryParam}`;
+      }
       try {
          setLoading(true);
-         const { data } = await axios.get("/all");
+         const { data } = await axios.get(dynamicURL);
 
          setCounteries(data);
       } catch (error) {
@@ -24,13 +33,12 @@ const HomeComponent = () => {
 
    useEffect(() => {
       getCounteries();
-   }, []);
+   }, [nameQueryParam]);
 
    return (
       <div className="home">
          <HomeHeader />
-         {loading && <h1>Loading</h1>}
-         {counteries && <CountriesList counteries={counteries} />}
+         {loading ? <h1>Loading</h1> : counteries && <CountriesList counteries={counteries} />}
       </div>
    );
 };
